ff442fb9de1e4e5d984359ce2e8020527bef75c3
Nazmul Hasan Shadin <nazmulhasanshadin000@gmail.com>
2025-10-04 11:07:29 +0600

    compare page has make responsive

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3001/api/v1",
+  // baseUrl: "http://localhost:3001/api/v1",
 
-  // baseUrl: "https://independent-shop.vercel.app/api/v1",
+  baseUrl: "https://independent-shop.vercel.app/api/v1",
 
   credentials: "include",
 
