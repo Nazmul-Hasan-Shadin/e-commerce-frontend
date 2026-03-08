7cdbcdf1b45bebd1404dade5c5b5b556f0ced7ad
Nazmul Hasan Shadin <nazmulhasanshadin000@gmail.com>
2026-03-09 01:45:31 +0600

    es lint error fixed

--- src/app/(withCommonLayout)/checkout/page.tsx
+++ src/app/(withCommonLayout)/checkout/page.tsx
@@ -65,7 +65,7 @@ const CheckoutPage = () => {
     }
   };
 
-  const handleChange = (e) => {
+  const handleChange = (e:any) => {
     const { name, value } = e.target;
 
     setOrderInfo((prev) => ({
